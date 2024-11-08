98a070b3b620897076ba2644af065d4dd0d5c61c
Koran1 <thekorani@gmail.com>
2024-11-08 17:10:46 +0900

    Update UserVO.java
    실수로 getter setter 가 2배

--- project1/src/main/java/com/ict/mytravellist/vo/UserVO.java
+++ project1/src/main/java/com/ict/mytravellist/vo/UserVO.java
@@ -5,187 +5,165 @@ public class UserVO {
 			userReg, userUpdReg, userConnReg, userFavor01, userFavor02, userFavor03, userEtc01, userEtc02, userEtc03,
 			userEtc04;
 
-	public String getUserPhone() {
-		return userPhone;
-	}
-
-	public void setUserPhone(String userPhone) {
-		this.userPhone = userPhone;
-	}
-
-	public String getN_userId() {
-		return n_userId;
-	}
-
-	public void setN_userId(String n_userId) {
-		this.n_userId = n_userId;
-	}
-
-	public String getK_userId() {
-		return k_userId;
-	}
-
-	public void setK_userId(String k_userId) {
-		this.k_userId = k_userId;
-	}
-
 	public String getUserIdx() {
 		return userIdx;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public String getN_userId() {
-		return n_userId;
-	}
-
-	public String getK_userId() {
-		return k_userId;
-	}
-
-	public String getUserPw() {
-		return userPw;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public String getUserPhone() {
-		return userPhone;
-	}
-
-	public String getUserMail() {
-		return userMail;
-	}
-
-	public String getUserAddr() {
-		return userAddr;
-	}
-
-	public String getUserLevel() {
-		return userLevel;
-	}
-
-	public String getUserReg() {
-		return userReg;
-	}
-
-	public String getUserUpdReg() {
-		return userUpdReg;
-	}
-
-	public String getUserConnReg() {
-		return userConnReg;
-	}
-
-	public String getUserFavor01() {
-		return userFavor01;
-	}
-
-	public String getUserFavor02() {
-		return userFavor02;
-	}
-
-	public String getUserFavor03() {
-		return userFavor03;
-	}
-
-	public String getUserEtc01() {
-		return userEtc01;
-	}
-
-	public String getUserEtc02() {
-		return userEtc02;
-	}
-
-	public String getUserEtc03() {
-		return userEtc03;
-	}
-
-	public String getUserEtc04() {
-		return userEtc04;
 	}
 
 	public void setUserIdx(String userIdx) {
 		this.userIdx = userIdx;
 	}
 
+	public String getUserId() {
+		return userId;
+	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
+	}
+
+	public String getN_userId() {
+		return n_userId;
 	}
 
 	public void setN_userId(String n_userId) {
 		this.n_userId = n_userId;
 	}
 
+	public String getK_userId() {
+		return k_userId;
+	}
+
 	public void setK_userId(String k_userId) {
 		this.k_userId = k_userId;
+	}
+
+	public String getUserPw() {
+		return userPw;
 	}
 
 	public void setUserPw(String userPw) {
 		this.userPw = userPw;
 	}
 
+	public String getUserName() {
+		return userName;
+	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	public String getUserPhone() {
+		return userPhone;
 	}
 
 	public void setUserPhone(String userPhone) {
 		this.userPhone = userPhone;
 	}
 
+	public String getUserMail() {
+		return userMail;
+	}
+
 	public void setUserMail(String userMail) {
 		this.userMail = userMail;
+	}
+
+	public String getUserAddr() {
+		return userAddr;
 	}
 
 	public void setUserAddr(String userAddr) {
 		this.userAddr = userAddr;
 	}
 
+	public String getUserLevel() {
+		return userLevel;
+	}
+
 	public void setUserLevel(String userLevel) {
 		this.userLevel = userLevel;
+	}
+
+	public String getUserReg() {
+		return userReg;
 	}
 
 	public void setUserReg(String userReg) {
 		this.userReg = userReg;
 	}
 
+	public String getUserUpdReg() {
+		return userUpdReg;
+	}
+
 	public void setUserUpdReg(String userUpdReg) {
 		this.userUpdReg = userUpdReg;
+	}
+
+	public String getUserConnReg() {
+		return userConnReg;
 	}
 
 	public void setUserConnReg(String userConnReg) {
 		this.userConnReg = userConnReg;
 	}
 
+	public String getUserFavor01() {
+		return userFavor01;
+	}
+
 	public void setUserFavor01(String userFavor01) {
 		this.userFavor01 = userFavor01;
+	}
+
+	public String getUserFavor02() {
+		return userFavor02;
 	}
 
 	public void setUserFavor02(String userFavor02) {
 		this.userFavor02 = userFavor02;
 	}
 
+	public String getUserFavor03() {
+		return userFavor03;
+	}
+
 	public void setUserFavor03(String userFavor03) {
 		this.userFavor03 = userFavor03;
+	}
+
+	public String getUserEtc01() {
+		return userEtc01;
 	}
 
 	public void setUserEtc01(String userEtc01) {
 		this.userEtc01 = userEtc01;
 	}
 
+	public String getUserEtc02() {
+		return userEtc02;
+	}
+
 	public void setUserEtc02(String userEtc02) {
 		this.userEtc02 = userEtc02;
+	}
+
+	public String getUserEtc03() {
+		return userEtc03;
 	}
 
 	public void setUserEtc03(String userEtc03) {
 		this.userEtc03 = userEtc03;
 	}
 
+	public String getUserEtc04() {
+		return userEtc04;
+	}
+
 	public void setUserEtc04(String userEtc04) {
 		this.userEtc04 = userEtc04;
 	}
+
+	
 }
